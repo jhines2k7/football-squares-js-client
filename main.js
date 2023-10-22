0d4f57e73a837f66844a533580fd08d92e4192e6
James Hines <jameshines10@gmail.com>
2023-10-22 15:30:08 +0000

    Players can claim squares

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ let socket = null;
 let contracts = {};
 let accounts = [];
 let web3 = null;
+let playerId = null;
+let gameIdH2 = null;
+let playerIdH3 = null;
+let gameNameH4 = null;
+let gameId = null;
+
+let heartbeatInterval;
 
 // create a click event handler called claim square
 // when a square is clicked, the user's address and the square's coordinates are sent to the server
@@ -12,16 +19,72 @@ let web3 = null;
 // the client will then display a message to the user that the square is not available
 function claimSquare(event) {
   var row = event.target.getAttribute('data-row');
-  var col = event.target.getAttribute('data-col');
+  var column = event.target.getAttribute('data-col');
 
-  console.log('Clicked cell at row ' + row + ', col ' + col);
+  console.log('Clicked cell at row ' + row + ', col ' + column);
 
-  event.target.textContent = "";
+  event.target.textContent = playerId;
+  event.target.removeEventListener('click', claimSquare);
+  event.target.style.backgroundColor = 'yellow';
+
+  socket.emit('claim_square', { row: row, column: column, player_id: playerId, game_id: gameId });
+}
+
+function selectTableCell(dataRow, dataColumn) {
+  return document.querySelector(`td[data-row='${dataRow}'][data-col='${dataColumn}']`);
+}
+
+function generateGamesList(arr, ul) {
+  // Loop through the array
+  for (let i = 0; i < arr.length; i++) {
+    // Create a new li for each item in the array
+    let li = document.createElement('li');
+
+    // Set the text content of the li to the name property of the current object
+    li.textContent = arr[i].name;
+
+    li.addEventListener('click', function () {
+      joinGame(arr[i].game_id);
+    });
+
+    // Append the li to the ul
+    ul.appendChild(li);
+  }
+}
+
+function joinGame(gameId) {
+  console.log("Clicked game with id: " + gameId);
+  socket.emit('join_game', { game_id: gameId, player_id: playerId });
 }
 
 function registerSocketIOEventListeners() {
   socket.on('heartbeat_response', (data) => {
     console.log(`Heartbeat received: ${JSON.stringify(data)}`);
+  });
+
+  socket.on('connected', (data) => {
+    console.log(`Player info: ${JSON.stringify(data.player)}`);
+    console.log(`Games list: ${JSON.stringify(data.games_list)}`);
+    const player = data.player;
+    playerId = player.id;
+    playerIdH3.textContent = `Player ID: ${playerId}`;
+
+    generateGamesList(data.games_list, document.getElementById('games'));
+  });
+
+  socket.on('game_joined', (game) => {
+    console.log(`Game joined: ${JSON.stringify(game)}`);
+    gameId = game.id;
+    gameIdH2.textContent = `Game ID: ${gameId}`;
+    gameNameH4.textContent = `Game Name: ${game.name}`;
+  });
+
+  socket.on('square_claimed', (data) => {
+    console.log(`Square claimed: ${JSON.stringify(data)}`);
+    const cell = selectTableCell(data.row, data.column);
+    cell.textContent = data.claimed_by;
+    cell.removeEventListener('click', claimSquare);
+    cell.style.backgroundColor = 'yellow';
   });
 }
 
@@ -69,6 +132,9 @@ async function convertUsdToEther(amountInUsd) {
 
 document.addEventListener('DOMContentLoaded', () => {
   (async () => {
+    gameIdH2 = document.querySelector('#header h2');
+    playerIdH3 = document.querySelector('#header h3');
+    gameNameH4 = document.querySelector('#header h4');
     /*
         while (!window.ethereum) {
           console.log('Waiting for MetaMask...');
@@ -108,6 +174,16 @@ document.addEventListener('DOMContentLoaded', () => {
       // Add the row to the table
       table.appendChild(row);
     }
+    socket = io('http://67.205.128.30:8000',
+      {
+        transports: ['websocket']
+      });
+
+    registerSocketIOEventListeners();
+
+    heartbeatInterval = setInterval(function () {
+      socket.emit('heartbeat', { pulse: 'ping' })
+    }, 20000); // Send heartbeat every 2 seconds
     /*
         if(typeof accounts[0] !== 'undefined') {
           socket = io('https://fsdev.generalsolutions43.com',
